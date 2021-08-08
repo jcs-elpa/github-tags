967d7d0480c197faea9aeaefd70753e365687d0d
JenChieh <jcs090218@gmail.com>
2021-08-08 17:08:13 +0800

    Bump version

--- github-tags.el
+++ github-tags.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Retrieve tags information through GitHub API
 ;; Keyword: github tags
-;; Version: 0.0.1
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24.3"))
 ;; URL: https://github.com/jcs-elpa/github-tags
 
